9e8459e6aff782cbc5b10a3b369a892723492ef3
Rob <bobby.rust121@gmail.com>
2025-02-17 20:22:09 -0500

    facebook page info retrieval done, started create post functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { config } from "../config/dotenv";
-import { FacebookLoginResponse } from "../types";
-import { exchangeCodeForAccessToken } from "../lib/facebook_api";
+import { getFacebookPagePicture, getFacebookPages } from "../lib/facebook_api";
 
 export function login(req: Request, res: Response) {
     const fbLoginUrl =
@@ -9,20 +8,24 @@ export function login(req: Request, res: Response) {
         "/dialog/oauth?" +
         `client_id=${config.FACEBOOK_APP_ID}` +
         `&redirect_uri=${encodeURIComponent(config.REDIRECT_URI)}` +
-        "&scope=pages_manage_posts,pages_show_list,email,public_profile" +
+        "&scope=pages_manage_metadata,pages_manage_posts,pages_show_list,email,public_profile,pages_manage_engagement,pages_read_engagement" +
         "&response_type=code";
 
     return res.redirect(fbLoginUrl);
 }
 
 export async function callback(req: Request, res: Response) {
-    console.log("In callback");
-    const code: string = req.query.code as string;
-    console.log("Got query: ", req.query);
-
+    const code = req.query.code as string;
     if (!code) console.error("No callback code found");
-    const response = await exchangeCodeForAccessToken(code);
 
-    res.status(200);
-    return res.send(response);
+    const pages = await getFacebookPages(code);
+    console.log(pages);
+
+    const pageId = pages[1].id;
+    const pageAccessToken = pages[1].access_token;
+
+    const pageInfo = await getFacebookPagePicture(pageId, pageAccessToken);
+    console.log("Got page info: ", pageInfo);
+
+    return res.status(200).json(pageInfo);
 }
--- src/lib/facebook_api.ts
+++ src/lib/facebook_api.ts
@@ -4,9 +4,7 @@ import { fetchJSON } from "./utils";
 /**
  * Returns a long-lived user access token (expires in ~60 days)
  */
-export async function exchangeCodeForAccessToken(
-    code: string,
-): Promise<string | Object> {
+export async function exchangeCodeForAccessToken(code: string): Promise<any> {
     const url =
         config.FACEBOOK_API_URL +
         "/oauth/access_token?" +
@@ -16,9 +14,99 @@ export async function exchangeCodeForAccessToken(
         `&code=${code}`;
 
     const response = await fetchJSON(url);
-    console.log("access token response: ", response);
-
-    return response;
+    return response.access_token;
 }
 
-export async function getFacebookPages(userId: string, userAccessToken: string);
+/**
+ * Gets a user's managed facebook pages
+ */
+export async function getFacebookPages(loginCode: string) {
+    const userAccessToken = await exchangeCodeForAccessToken(loginCode);
+    const userId = await getUserId(userAccessToken);
+    const url =
+        config.FACEBOOK_API_URL +
+        `/${userId}/accounts` +
+        `?access_token=${userAccessToken}`;
+
+    const pagesData = await fetchJSON(url);
+    return pagesData.data || [];
+}
+
+/**
+ * Gets a facebook page's profile picture
+ */
+export async function getFacebookPagePicture(
+    pageId: string,
+    pageAccessToken: string,
+) {
+    const url =
+        config.FACEBOOK_API_URL +
+        `/${pageId}?access_token=${pageAccessToken}&fields=picture`;
+
+    const response = await fetchJSON(url);
+    return response.picture.data;
+}
+
+/**
+ * Gets an app access token
+ * App access tokens are sensitive and must never be exposed client side
+ */
+export async function getAppAccessToken(): Promise<string> {
+    const url =
+        config.FACEBOOK_API_URL +
+        "/oauth/access_token" +
+        `?client_id=${config.FACEBOOK_APP_ID}` +
+        `&client_secret=${config.FACEBOOK_APP_SECRET}` +
+        "&grant_type=client_credentials";
+
+    const response = await fetchJSON(url);
+    return response.access_token;
+}
+
+/**
+ * Returns information about an access token
+ */
+export async function inspectAccessToken(accessToken: string): Promise<any> {
+    const appAccessToken = await getAppAccessToken();
+    const url =
+        config.FACEBOOK_API_URL +
+        "/debug_token" +
+        `?input_token=${accessToken}` +
+        `&access_token=${appAccessToken}`;
+
+    const response = await fetchJSON(url);
+    return response.data;
+}
+
+/**
+ * Gets the user ID associated with the access token
+ */
+export async function getUserId(accessToken: string): Promise<string> {
+    const accessTokenInspection = await inspectAccessToken(accessToken);
+    return accessTokenInspection.user_id;
+}
+
+/**
+ * Creates a new facebook post for a page that publishes at scheduled_publish_time
+ * @param scheduled_publish_time Must be between 30 minutes and 30 days from the time of the request
+ * The scheduled publish time must be an integer UNIX timestamp [in seconds],
+ * an ISO 8061 timestamp string, or any string parsable by PHP's strtotime()
+ */
+export async function createPost(
+    pageId: string,
+    message: string,
+    scheduled_publish_time: string,
+) {
+    const url = config.FACEBOOK_API_URL + `/${pageId}/feed`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: message,
+            published: false,
+            scheduled_publish_time: scheduled_publish_time,
+        }),
+    });
+}
